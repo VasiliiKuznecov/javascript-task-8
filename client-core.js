c2a9216e225c74519244901b871e0b2fbade0cd2
Кузнецов Василий <kuznecov_vasilii@mail.ru>
2017-12-04 20:27:02 +0500

    Update client-core.js

--- client-core.js
+++ client-core.js
@@ -5,7 +5,6 @@ module.exports.isStar = true;
 
 function execute() {
     // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
 
     return Promise.resolve('Это строка будет выведена на консоль');
 }
